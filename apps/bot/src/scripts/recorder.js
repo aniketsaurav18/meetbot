889cec8fe08fdb78463c725f9727bb9e5d2cb6d6
Saurav Sharma <aniketsaurav18@gmail.com>
2026-04-23 08:56:44 +0530

    added function in recorder.js

--- apps/bot/src/scripts/recorder.js
+++ apps/bot/src/scripts/recorder.js
@@ -6,14 +6,35 @@
     function log(e){
         console.log("[Recorder Script]: ", e);
     }
-    let audioconfig;
+    let audiocontext;
     let mediarecorder;
+    let dest;
     function findMediaElement(){
         try{
             const element = document.querySelectorAll("audio");
-            
+            if(element.length == 0){
+                log("no media element found");
+                return null;
+            }
+            return element;
         }catch(e){
             console.log(e)
+        }
+    }
+
+    function mergeMediaElement(mediaElements){
+        try{
+            audiocontext = new AudioContext();
+            dest = audiocontext.createMediaStreamDestination();
+            mediaElements.forEach((ele) => {
+                let mediaStream = ele.srcObject;
+                if(mediaStream instanceof MediaStream && mediaStream.getAudioTracks().length > 0){
+                    let source = audiocontext.createMediaStreamSource(mediaStream);
+                    source.connect(dest);
+                }
+            })
+        }catch(e){
+            log(e);
         }
     }
 })();
